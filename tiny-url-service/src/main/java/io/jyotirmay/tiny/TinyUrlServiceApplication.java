0d16c96a43cffc57be2ee98fbf11c6ef1f59108c
jyotirmaykumar <jyotirmaykumar@outlook.com>
2024-03-30 04:47:16 +0530

    Added base project for tiny url service

---
+++ tiny-url-service/src/main/java/io/jyotirmay/tiny/TinyUrlServiceApplication.java
@@ -0,0 +1,12 @@
+package io.jyotirmay.tiny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyUrlServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TinyUrlServiceApplication.class, args);
+    }
+}
